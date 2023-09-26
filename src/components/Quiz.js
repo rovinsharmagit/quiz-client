d840920ef6e31459bb651f6e73f7f723f9a26529
rovinsharmagit <internetsharma45@gmail.com>
2023-09-26 18:53:55 +0530

    Working With Question Components

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,7 +1,7 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import useStateContext from '../hooks/useStateContext'
-import { ENDPOINTS, createAPIEndpoint } from '../api'
-import { Card, CardContent, CardHeader, List, ListItemButton, Typography } from '@mui/material'
+import { BASE_URL, ENDPOINTS, createAPIEndpoint } from '../api'
+import { Box, Card, CardContent, CardHeader, CardMedia, LinearProgress, List, ListItemButton, Typography } from '@mui/material'
 import { getFormatedTime } from '../helper'
 
 export default function Quiz() {
@@ -9,44 +9,78 @@ export default function Quiz() {
   const [qns, setQns] = useState([])
   const [qnIndex, setQnIndex] = useState(0)
   const [timeTaken, setTimeTaken] = useState(0)
+  const {context, setContext} = useStateContext()
 
-  let timer;
+  const timerRef = useRef(null);
+
+  let timer = null;
 
   const startTimer = () => {
-   const startTime = performance.now();
-   timer = setInterval(() => {
-     const currentTime = performance.now();
-     const elapsedSeconds = Math.floor((currentTime - startTime) / 1000);
-     setTimeTaken(elapsedSeconds);
-   }, 1000);
-   };
+   if (timerRef.current === null) { // Check if the timer is already running
+     const startTime = performance.now();
+     timerRef.current = setInterval(() => {
+       const currentTime = performance.now();
+       const elapsedSeconds = Math.floor((currentTime - startTime) / 1000);
+       setTimeTaken(elapsedSeconds);
+     }, [1000]); // Use 1000 milliseconds (1 second) as the interval
+   }
+ };
 
   useEffect(() => {
      createAPIEndpoint(ENDPOINTS.question)
      .fetch()
      .then(res => {
-           setQns(res.data)
-           startTimer()
+           setQns(res.data);
+           startTimer();
      })
      .catch(err => {console.log(err);})
-      return () => {clearInterval(timer)}
+     return () => {
+      if (timerRef.current !== null) {
+        clearInterval(timerRef.current);
+        timerRef.current = null; 
+      }
+    };
   },[]) 
+
+  const updateAnswer = (qnId,optionIdx) => {
+   const temp =[...context.selectedOption]
+      temp.push({
+      qnId,
+      selected:optionIdx
+   })
+  
+   if (qnIndex < 4){
+      setContext({selectedOption:[...temp]})
+      setQnIndex(qnIndex+1)
+   }
+   else{ setContext({selectedOption:[...temp], timeTaken})
+   }
+  }
 
   return (
       qns.length !== 0
       ?<Card
-         sx={{maxWidth:640, mx:'auto', mt:'5'}} >
+         sx={{maxWidth:640, mx:'auto', mt:'5',
+         '& .MuiCardHeader-action':{m:0, alignSelf:'center'}}} >
             <CardHeader
             title={ 'Question ' + (qnIndex + 1) + ' of 5 ' }
             action={<Typography>{getFormatedTime(timeTaken)}</Typography>}/>
+            <Box>
+               <LinearProgress variant="determinate" value={(qnIndex+1)*100/5} />
+            </Box>
+            {qns[qnIndex].imageName != null
+               ? <CardMedia
+                   component="img"
+                   image={BASE_URL + 'images/' + qns[qnIndex].imageName}
+                   sx={{ width: 'auto', m: '10px auto' }} />
+            :null}
          <CardContent>
             <Typography variant="h6">
                {qns[qnIndex].qnInWords}
             </Typography>
             <List>
                {qns[qnIndex].options.map((item, idx) =>
-                 <ListItemButton key={idx}
-                 disappleRipple>
+                 <ListItemButton key={idx} onClick={()=>updateAnswer(qns[qnIndex].qnId,idx)}>
                     <div>
                       <b>{String.fromCharCode(65+idx)+" . "}</b> {item}
                     </div>
